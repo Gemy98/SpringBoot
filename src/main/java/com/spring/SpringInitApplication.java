b136d4016213ecb182d12458a49d49edc66ee2e1
Gemy98 <mohamedgamal19981998@gmail.com>
2023-05-24 06:52:43 +0300

    Add devTools dependency for build

--- src/main/java/com/spring/SpringInitApplication.java
+++ src/main/java/com/spring/SpringInitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringInitApplication {
 
 	public static void main(String[] args) {
+		//have the tomcat server and have the container
 		SpringApplication.run(SpringInitApplication.class, args);
 	}
 
--- src/main/java/com/spring/controller/TestingController.java
+++ src/main/java/com/spring/controller/TestingController.java
@@ -1,5 +1,6 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 //localhost:8080/api
 public class TestingController {
+    @Value("${admin.name}")
+    private String name ;
 
     //localhost:8080/api/name
     @GetMapping("/name")
     public String getHome(){
 
-        return "i am Gemy";
+        return "i am  :"+name;
+    }
+
+    //localhost:8080/api/age
+    @GetMapping("/age")
+    public String getAge(){
+
+        return "25";
+    }
+
+    //localhost:8080/api/address
+    @GetMapping("/address")
+    public String getAddress(){
+
+        return "Cairo";
+    }
+
+    //localhost:8080/api/phone
+    @GetMapping("/phone")
+    public String getPhone(){
+
+        return "01068440098";
     }
 
 }
